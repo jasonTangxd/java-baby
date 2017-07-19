c6d6ed1b78b7b2d0ca60c823e024da8477ceb838
jasonTangxd <jason.tangxd@gmail.com>
2017-07-19 11:51:29 +0800

    序列化

---
+++ src/main/java/com/xxo/serial/SerialReadTest.java
@@ -0,0 +1,42 @@
+package com.xxo.serial;
+
+import java.io.*;
+
+/**
+ *
+ * 反序列化取出对象
+ *
+ * Created by xiaoxiaomo on 2012/4/27.
+ */
+public class SerialReadTest {
+
+    public static void main(String[] args) {
+
+        //反序列化取出对象
+        String path = "d:\\person.txt";
+        ObjectInputStream inputStream = null;
+
+        try {
+
+            //从磁盘中读入
+            inputStream = new ObjectInputStream(new FileInputStream(path));
+            Person person = (Person) inputStream.readObject();
+
+            System.out.println("Name:" + person.getName() + " Age:"+ person.getAge());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != inputStream)
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+
+    }
+
+}
