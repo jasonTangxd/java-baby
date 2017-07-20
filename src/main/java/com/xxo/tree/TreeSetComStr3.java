22320814169192d574d7c1f32ae153507f8525b9
jasonTangxd <jason.tangxd@gmail.com>
2017-07-20 15:14:15 +0800

    package remove to xiaoxiaomo

--- src/main/java/com/xxo/tree/TreeSetComStr3.java
+++
@@ -1,8 +0,0 @@
-package com.xxo.tree;
-
-/**
- * Created by xiaoxiaomo on 2012/4/5.
- */
-public interface TreeSetComStr3 {
-    void Compares();
-}
--- src/test/java/com/xxo/day21/test/Inter.java
+++
@@ -1,6 +0,0 @@
-package com.xxo.day21.test;
-
-public interface Inter {
-	void show();
-
-}
