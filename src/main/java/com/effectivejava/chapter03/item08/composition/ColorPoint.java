a319e942e6ff1116f643c4e663303a0d83d2220b
jasonTangxd <jason.tangxd@gmail.com>
2017-07-24 11:19:49 +0800

    study 第三章节

--- src/main/java/com/effectivejava/chapter03/item08/composition/ColorPoint.java
+++ src/main/java/com/effectivejava/chapter03/item08/composition/ColorPoint.java
@@ -32,3 +32,20 @@ public class ColorPoint {
 		return point.hashCode() * 33 + color.hashCode();
 	}
 }
+
+//高质量equals方法：
+//		1、使用==操作符检查“参数是否为这个对象的引用”。
+//		2、使用instanceof操作符检查“参数是否为正确的类型”。
+//		3、把参数转换成正确的类型。
+//		4、对于该类的每个“关键（significant）”域，检查参数中的域是否与该对象中对应的域相匹配。
+//		这点myeclipse自动生成的并不完全完美，myeclipse生成如下：
+//		else if (!name.equals(other.name))
+//		return false;
+//		但是，这条是要求你改成：
+//		else if (name != other.name && !name.equals(other.name))
+//		return false;
+//		5、当你编写完成了equals方法之后，应该会问自己三个问题：他是否的对称的、传递的、一致的。
+//		（覆盖equals时总要覆盖hashcode；不要企图让equals方法过于智能；不要将equals声明中的Object对象替换为其他的类型。）
+//
+//
+//		尽量不要省略@Override。
